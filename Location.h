d90ece4ce2a4006fea7d4e24a6e3b413ae8a08e5
mjesguerra <mjesguerra@fbi.gov>
2025-05-20 14:11:37 -0700

    initialize location.h to match UML diagram

---
+++ Location.h
@@ -0,0 +1,34 @@
+#ifndef LOCATION_H
+#define LOCATION_H
+
+#include <string>
+
+enum Direction {
+    NORTH, EAST, SOUTH, WEST
+};
+
+class Location {
+    public:
+        Location();
+        Location(const std::strings& locName, bool isTheExit);
+
+        std::string getName() const;
+        std::string getDescription() const;
+
+        bool hasNeighbor(Direction dir) const;
+        Location* getNeighbor(Direction dir) const;
+        void setNeighbor(Direction dir, Location* neighborLoc);
+
+        bool isExit() const;
+        void visit();
+        bool isVisited() const;
+
+    private:
+        std::string name;
+        bool visited;
+        bool exit;
+        Location* neighbors[4];
+
+};
+
+#endif
